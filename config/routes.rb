204cb88109ee1c4b434b9bd8513dbd37da1b238f
Malik Bouaoudia <malik@bouaoudia.com>
2022-11-17 15:01:42 +0000

    added show feature for specific list id

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,12 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get    "lists",          to: "lists#index"
+
+  # Read one - The `show` route needs to be *after* `new` route.
+  get    "lists/:id",      to: "lists#show", as: :list
+
+  # NEW
+  # Create
+  # get    "lists/new",      to: "lists#new", as: :new_list
+  # post   "lists",          to: "lists#create"
 end
