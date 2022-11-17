2a8d1dfaa3bd96c3fd50a93217d0b985dd4575a5
Malik Bouaoudia <malik@bouaoudia.com>
2022-11-17 15:09:29 +0000

    successfully added create functionality to list controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
   # root "articles#index"
   get    "lists",          to: "lists#index"
 
-  # Read one - The `show` route needs to be *after* `new` route.
-  get    "lists/:id",      to: "lists#show", as: :list
-
   # NEW
   # Create
-  # get    "lists/new",      to: "lists#new", as: :new_list
-  # post   "lists",          to: "lists#create"
+  get    "lists/new",      to: "lists#new", as: :new_list
+  post   "lists",          to: "lists#create"
+
+  # Read one - The `show` route needs to be *after* `new` route.
+  get    "lists/:id",      to: "lists#show", as: :list
 end
